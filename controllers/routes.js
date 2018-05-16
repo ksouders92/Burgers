70280da30fd993365e9d6e1b535b527556d8da76
Kathleen Souders <ksouders@knights.ucf.edu>
2018-05-16 15:20:08 -0400

    create function

--- controllers/routes.js
+++ controllers/routes.js
@@ -11,7 +11,13 @@ router.get("/", function (req, res){
 
 router.post('/burgers/update', function(req, res){
     burger.update(req.body.burger_id, function(result){
-        console.log(result);
+        //console.log(result);
+        res.redirect('/');
+    });
+});
+
+router.post('/burgers/create', function(req, res){
+    burger.create(req.body.burger_name, function(result){
         res.redirect('/');
     });
 });

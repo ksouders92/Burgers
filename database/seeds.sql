cd1ef86fe719b4e630419a945f4b41eae90efd0a
Kathleen Souders <ksouders@knights.ucf.edu>
2018-05-16 01:55:30 -0400

    create database schema and seeds

---
+++ database/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nothing Compares 2 Blue (Cheese) Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dont You Four Cheddar Bout Me Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('As Gouda As it Gets Burgers', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Total Eclipse of the Havarti Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('A Good Manchego is Hard to Find Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ive Created a Muenster Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Captain Pepper Jack Sparrow Burgers', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Little Miss Bunshine Burger', false);

e7e782644b62d2cf799e575418c1fbaeaa024618
Kathleen Souders <ksouders@knights.ucf.edu>
2018-05-22 02:14:26 -0400

    css

--- database/schema.sql
+++ database/schema.sql
@@ -9,11 +9,4 @@ CREATE TABLE burgers (
  PRIMARY KEY (id)
 );
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Nothing Compares 2 Blue (Cheese) Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Dont You Four Cheddar Bout Me Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('As Gouda as it Gets Burgers', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Total Eclipse of the Havarti Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('A Good Manchego is Hard to Find Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Ive Created a Muenster Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Captain Pepper Jack Sparrow Burgers', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Little Miss Bunshine Burger', false);
+
--- server.js
+++ server.js
@@ -3,18 +3,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
+var path = require('path');
+var favicon = require('serve-favicon');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-
-
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+app.use(favicon(path.join(__dirname,'public', 'assets', 'images','favicon.jpg')));
 
 app.use(methodOverride('__method'));
 app.engine('handlebars', exphbs({
@@ -24,6 +26,11 @@ app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/routes.js');
 app.use("/", routes);
+
+app.get('/css', function (req, res){
+    res.sendFile(path.join(__dirname, '/public/assets/css/style.css'));
+  
+  })
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {

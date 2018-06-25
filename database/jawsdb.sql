43ce3c0309150f82b0592f10f1895422a760be86
Kathleen Souders <ksouders@knights.ucf.edu>
2018-06-25 17:17:34 -0400

    jawsdb connection

---
+++ database/jawsdb.sql
@@ -0,0 +1,18 @@
+USE al5d7qhmptx3xatf;
+
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers (
+ id int NOT NULL auto_increment,
+ burger_name varchar(100) NOT NULL,
+ devoured BOOL DEFAULT false,
+ PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nothing Compares 2 Blue (Cheese) Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dont You Four Cheddar Bout Me Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('As Gouda as it Gets Burgers', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Total Eclipse of the Havarti Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('A Good Manchego is Hard to Find Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ive Created a Muenster Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Captain Pepper Jack Sparrow Burgers', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Little Miss Bunshine Burger', false);
